ab32336be645faf4bac3afe2859b4e2589d11dfe
kspohodyaev <konstantinpohodyaev@yandex.ru>
2026-09-14 14:26:01 -0700

    Добавил проверку на нулевой знаменатель

--- fraction.py
+++ fraction.py
@@ -1,4 +1,8 @@
 class Fraction:
     def __init__(self, ch, zn):
+        if zn == 0:
+            raise ValueError(
+                f'Знаменатель не может равняться 0!'
+            )
         self.ch = ch
         self.zn = zn
