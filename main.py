25293aeb20f263d5eb5d95fbd370279054a713f2
kspohodyaev <konstantinpohodyaev@yandex.ru>
2026-09-14 16:05:52 -0700

    Добавил логирование

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
 """MAIN файл - основная логика программы."""
 
+import logging
+
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+
 def main():
-  pass
+    logger.info('START')
+    # TODO: основная программа
+    logger.info('STOP')
 
 
 if __name__ == '__main__':
-  main()
+    main()
