893f5c006f6213434c933b33fc77e2e2f9625c0e
kspohodyaev <konstantinpohodyaev@yandex.ru>
2026-09-14 15:40:07 -0700

    Revert "Добавил проверку на некорректный размер матрицы"
    This reverts commit 71fc365cdb721f09ade22b24321f9659b5fa912d.

--- matrix.py
+++ matrix.py
@@ -1,10 +1,5 @@
 class Matrix:
     def __init__(self, size):
-        if size < 1:
-            raise ValueError(
-                f'Кол-во строк не может быть меньше 1: {size}'
-            )
-
         self.matrix = [
             [0 for _ in range(size)]
             for _ in range(size)
